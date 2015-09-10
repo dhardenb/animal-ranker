bf8149f79875ee811b4c16f04d1f327eb6a06c7a
David Hardenbrook <davehardenbrook@yahoo.com>
2015-09-10 13:33:04 -0500

    First Commit

---
+++ client/templates/animals/animal.js
@@ -0,0 +1,17 @@
+Template.animal.events({
+	'click #edit': function(e) {
+		e.preventDefault();
+		animal = $(e.target).closest('.animal')
+    animalId = animal.attr('data-id')
+    ModalHelper.openModalFor(animalId);
+  },
+  'click #delete': function(e) {
+    e.preventDefault();
+
+    Meteor.call('deleteAnimal', this, function(error, result) {
+      if (error) {
+        alert(error);
+      } 
+    });
+  }
+});
